db12ae3e136e0fcfe1f53e57c3e64317a0bf4037
loren950603 <loren840603@gmail.com>
2022-07-20 21:25:36 -0400

    complete project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log('hi')
+const $input = $('input');
+const $plus = $('#plus');
+const $minus = $('#minus');
+const $display = $('#display');
+
+function handlePlusClick(e) {
+    let inputValue = parseInt($input.val());
+    let displayValue = parseInt($display.html());
+    let result = inputValue + displayValue;
+    $display.html(result.toString());
+    if (result < 0) {
+        $display.css({'color': 'red'})
+    } else {
+        $display.css({'color': 'black'})
+    }
+}
+
+function handleMinusClick(e) {
+    let inputValue = parseInt($input.val());
+    let displayValue = parseInt($display.html());
+    let result = displayValue - inputValue;
+    $display.html(result.toString());
+    if (result < 0) {
+        $display.css({'color': 'red'})
+    } else {
+        $display.css({'color': 'black'})
+    }
+}
+
+
+$plus.click(handlePlusClick);
+
+$minus.click(handleMinusClick);
